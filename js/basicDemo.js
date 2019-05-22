8d06299293ba5db9534950cf3b49a89cbf0aa456
Daniel Velasquez <anemoleiros@gmail.com>
2019-05-21 20:52:14 -0400

    Replaced object spread replaced with object assign

--- js/basicDemo.js
+++ js/basicDemo.js
@@ -8,14 +8,16 @@ function createDemoEffect(options) {
   const transitionEffect = new GridToFullscreenEffect(
     document.getElementById("app"),
     document.getElementById("itemsWrapper"),
-    {
-      scrollContainer: window,
-      onToFullscreenStart: ({ index }) => {},
-      onToFullscreenFinish: ({ index }) => {},
-      onToGridStart: ({ index }) => {},
-      onToGridFinish: ({ index, lastIndex }) => {},
-      ...options
-    }
+    Object.assign(
+      {
+        scrollContainer: window,
+        onToFullscreenStart: ({ index }) => {},
+        onToFullscreenFinish: ({ index }) => {},
+        onToGridStart: ({ index }) => {},
+        onToGridFinish: ({ index, lastIndex }) => {}
+      },
+      options
+    )
   );
 
   return transitionEffect;
